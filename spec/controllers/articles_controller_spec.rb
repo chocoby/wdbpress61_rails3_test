571fc1c402b2e8e65a07b2748e906d8fe7bf03df
chocoby <chocoby@gmail.com>
2012-04-24 01:01:12 +0900

    p64 controllers/articles_controller_spec.rb

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,25 @@
+# coding: utf-8
+
 require 'spec_helper'
 
-describe ArticlesController do
-
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
+describe "GET 'index'" do
+  before do
+    @article1 = Article.create(
+      title: "Article 1",
+      body: "Hello"
+    )
+    @article2 = Article.create(
+      title: "Article 2",
+      body: "World"
+    )
+    get 'index'
   end
 
+  it "should be successful" do
+    response.should be_success
+  end
+
+  it "登録されている記事をすべて返すこと" do
+    assigns[:articles].should =~ [@article1, @article2]
+  end
 end
