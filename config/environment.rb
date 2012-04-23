50b935deae9995edb796bc2c15c3c9abbdd4bc90
chocoby <chocoby@gmail.com>
2012-04-24 00:23:56 +0900

    p60 initial commit.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Wdbpress61Blog::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdbpress61Blog::Application.config.secret_token = '304bfcb8bc1270d5637d7b0cb0d34ffcb09191169d30f1fd927135cd7f41b996fff281dec11bf4f4f279e2f516f8a8dcd603fef7444a8018af9efed6917421b7'
