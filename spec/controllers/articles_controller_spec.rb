2fbd173ebcc1a70ef56fc4bb7354c519b828d5ce
chocoby <chocoby@gmail.com>
2012-04-24 01:05:54 +0900

    p64 describe ArticlesController は必要

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,24 +2,26 @@
 
 require 'spec_helper'
 
-describe "GET 'index'" do
-  before do
-    @article1 = Article.create(
-      title: "Article 1",
-      body: "Hello"
-    )
-    @article2 = Article.create(
-      title: "Article 2",
-      body: "World"
-    )
-    get 'index'
-  end
+describe ArticlesController do
+  describe "GET 'index'" do
+    before do
+      @article1 = Article.create(
+        title: "Article 1",
+        body: "Hello"
+      )
+      @article2 = Article.create(
+        title: "Article 2",
+        body: "World"
+      )
+      get 'index'
+    end
 
-  it "should be successful" do
-    response.should be_success
-  end
+    it "should be successful" do
+      response.should be_success
+    end
 
-  it "登録されている記事をすべて返すこと" do
-    assigns[:articles].should =~ [@article1, @article2]
+    it "登録されている記事をすべて返すこと" do
+      assigns[:articles].should =~ [@article1, @article2]
+    end
   end
 end
